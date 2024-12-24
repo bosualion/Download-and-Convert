28470eb3a0c43f2f74a2d8fb682e4bf8dcab96f8
Hoài Ngữ <hoaingu02@gmail.com>
2024-12-24 17:32:26 +0700

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import streamlit as st
-st.title("Download and Convert")
-l,m,r = st.columns(3)
-m.image('Img.png',width=400)
-left, right = st.columns(2)
-if left.button("Download",use_container_width=True):
-    st.write("[YouTube](https://y2mate.bet)")
-    st.write("[Facebook](https://taivideo.vn)")
-if right.button("Convert",use_container_width=True):
-    st.write("https://cloudconvert.com/")
+with st.sidebar:
+    username = st.text_input("Username: ")
+    password = st.text_input("Password: ")
+if username == "Le Tan Loi" and password == "051323":
+    st.title("Download and Convert")
+    l,m,r = st.columns(3)
+    m.image('Img.png',width=400)
+    left, right = st.columns(2)
+    if left.button("Download",use_container_width=True):
+        st.write("[YouTube](https://y2mate.bet)")
+        st.write("[Facebook](https://taivideo.vn)")
+    if right.button("Convert",use_container_width=True):
+        st.write("https://cloudconvert.com/")
